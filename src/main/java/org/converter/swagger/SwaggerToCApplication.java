7e8d7b434b5d027c7051bc07da8737a763d3bf16
satyesht <satyesh.cse@gmail.com>
2022-12-30 16:05:50 -0600

    [DEV] Initial Commit

---
+++ src/main/java/org/converter/swagger/SwaggerToCApplication.java
@@ -0,0 +1,13 @@
+package org.converter.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerToCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerToCApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/converter/swagger/SwaggerToCApplicationTests.java
@@ -0,0 +1,13 @@
+package org.converter.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerToCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
